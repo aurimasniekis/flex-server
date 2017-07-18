253d7330463a6f6f6963ae318f141c9d5eff16b1
Aurimas Niekis <aurimas@niekis.lt>
2017-07-18 20:32:55 +0100

    Refactored routing.php file

--- routing.php
+++ routing.php
@@ -1,9 +1,3 @@
 <?php
 
-if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER['REQUEST_URI'])) {
-    return false;
-}
-
-file_put_contents("php://stdout", $_SERVER['REQUEST_URI'] . PHP_EOL);
-
 include __DIR__ . '/public/index.php';
